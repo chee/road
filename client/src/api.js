2d44a552a51527beaa60afffa6a401f108a3afed
chee <chee@snaek.org>
2017-04-26 03:34:58 +0100

    move api file and add peerfetcher

--- client/src/api.js
+++ client/src/api.js
@@ -1,17 +1,24 @@
 module.exports = {
 	getList,
-	addPicture
+	addPicture,
+	getPeers
 }
 
 const API_ROOT = 'http://localhost:9990'
+const PUBLIC = 'public'
 
 function getList(channel) {
-	return fetch(`${API_ROOT}/${channel}`)
+	return fetch(`${API_ROOT}/channels/${channel}`)
 		.then(response => response.json())
 }
 
-function addPicture(channel, sha) {
-	return fetch(`${API_ROOT}/${channel}`, {
+function getPeers() {
+	return fetch(`${API_ROOT}/peers`)
+		.then(response => response.json())
+}
+
+function addPicture(channel = PUBLIC, sha) {
+	return fetch(`${API_ROOT}/channels/${channel}`, {
 		method: 'POST',
 		headers: {'content-type': 'application/json'},
 		body: JSON.stringify({sha})
