1b839b56e3b7e29769a50697b691bdb91a5278a1
chee <chee@snake.dog>
2017-09-13 22:35:23 +0100

    switch to standard.js #server

--- server/index.js
+++ server/index.js
@@ -8,94 +8,94 @@ const peerPort = process.env.PEER_PORT || 9991
 
 const peers = {}
 const peerServer = PeerServer({
-	port: peerPort
+  port: peerPort
 })
 
 const channels = {
-	public: [
-		'8947268b7aabcda5844c1a73c273c15630a95ed8fcf5332b5d380ab5bcb53bc6'
-	],
-	test: [
-		'8947268b7aabcda5844c1a73c273c15630a95ed8fcf5332b5d380ab5bcb53bc6',
-		'8947268b7aabcda5844c1a73c273c15630a95ed8fcf5332b5d380ab5bcb53bc6'
-	]
+  public: [
+    '8947268b7aabcda5844c1a73c273c15630a95ed8fcf5332b5d380ab5bcb53bc6'
+  ],
+  test: [
+    '8947268b7aabcda5844c1a73c273c15630a95ed8fcf5332b5d380ab5bcb53bc6',
+    '8947268b7aabcda5844c1a73c273c15630a95ed8fcf5332b5d380ab5bcb53bc6'
+  ]
 }
 
 app.use((request, response, next) => {
-	response.header('access-control-allow-origin', '*')
-	response.header(
-		'access-control-allow-headers',
-		'origin, x-requested-with, content-type, accept'
-	)
-	next()
+  response.header('access-control-allow-origin', '*')
+  response.header(
+    'access-control-allow-headers',
+    'origin, x-requested-with, content-type, accept'
+  )
+  next()
 })
 
 app.use(body.json())
 
 const get = (request, response) => {
-	const {channel} = request.params
-	response.statusMessage = 'here you go'
-	if (!channels[channel]) {
-		channels[channel] = []
-	}
-	response.send([...channels[channel]].reverse())
+  const {channel} = request.params
+  response.statusMessage = 'here you go'
+  if (!channels[channel]) {
+    channels[channel] = []
+  }
+  response.send([...channels[channel]].reverse())
 }
 
 app.get('/channels/:channel', get)
 
 const noSha = response => {
-	response.statusMessage = 'oh dear'
-	response.status(400)
-	response.send({error: 'no sha'})
+  response.statusMessage = 'oh dear'
+  response.status(400)
+  response.send({error: 'no sha'})
 }
 
 const sameSha = response => {
-	response.statusMessage = 'please no'
-	response.status(409)
-	response.send({error: 'sorry that\'s the same image as before'})
+  response.statusMessage = 'please no'
+  response.status(409)
+  response.send({error: 'sorry that\'s the same image as before'})
 }
 
 const shaTooLong = response => {
-	response.statusMessage = 'awk no'
-	response.status(400)
-	response.send({error: 'sha too long'})
+  response.statusMessage = 'awk no'
+  response.status(400)
+  response.send({error: 'sha too long'})
 }
 
 const post = (request, response) => {
-	const {params: {channel}, body: {sha}} = request
+  const {params: {channel}, body: {sha}} = request
 
-	if (!channels[channel]) {
-		channels[channel] = []
-	}
+  if (!channels[channel]) {
+    channels[channel] = []
+  }
 
-	const list = channels[channel]
+  const list = channels[channel]
 
-	if (!sha) return noSha(response)
-	if (sha == list[list.length - 1]) return sameSha(response)
-	if (sha.length > 64) return shaTooLong(response)
+  if (!sha) return noSha(response)
+  if (sha === list[list.length - 1]) return sameSha(response)
+  if (sha.length > 64) return shaTooLong(response)
 
-	response.statusMessage = 'hey thanks'
-	response.status(200)
-	list.push(sha)
-	response.send([...list].reverse())
+  response.statusMessage = 'hey thanks'
+  response.status(200)
+  list.push(sha)
+  response.send([...list].reverse())
 }
 
 app.post('/channels/:channel', post)
 
 app.get('/peers', (request, response) => {
-	response.statusMessage = 'peers here!'
-	response.status(200)
-	response.send(Object.keys(peers))
+  response.statusMessage = 'peers here!'
+  response.status(200)
+  response.send(Object.keys(peers))
 })
 
 peerServer.on('connection', id => {
-	console.log(id, 'connect')
-	peers[id] = id
+  console.log(id, 'connect')
+  peers[id] = id
 })
 
 peerServer.on('disconnect', id => {
-	console.log(id, 'disconnect')
-	delete peers[id]
+  console.log(id, 'disconnect')
+  delete peers[id]
 })
 
 console.log(`listening on prot ${port}`)
